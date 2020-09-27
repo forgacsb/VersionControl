3afa3df7ad033e04f83827477a4b3b62ef5e43e1
Benjámin Forgács <benjamin.forgacs@stud.uni-corvinus.hu>
2020-09-27 16:36:05 +0200

    list setup

--- gyak2_CK90DC/gyak2_CK90DC/Form1.cs
+++ gyak2_CK90DC/gyak2_CK90DC/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak2_CK90DC.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,13 +13,28 @@ namespace gyak2_CK90DC
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
             InitializeComponent();
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName; 
             button1.Text = Resource1.Add; 
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }

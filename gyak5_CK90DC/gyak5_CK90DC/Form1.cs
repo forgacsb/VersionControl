3002b5e02390eeb48880ab271adbe3e72e96ccce
Benjámin Forgács <benjamin.forgacs@stud.uni-corvinus.hu>
2020-10-11 20:20:16 +0200

    download data from server to DGV

--- gyak5_CK90DC/gyak5_CK90DC/Form1.cs
+++ gyak5_CK90DC/gyak5_CK90DC/Form1.cs
@@ -12,9 +12,13 @@ namespace gyak5_CK90DC
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }

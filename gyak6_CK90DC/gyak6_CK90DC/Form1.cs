6c10efc3b55b2ebee726ce758d1c2291ec231141
Benjámin Forgács <forgacsb@outlook.hu>
2020-10-18 12:34:47 +0200

    request creation

--- gyak6_CK90DC/gyak6_CK90DC/Form1.cs
+++ gyak6_CK90DC/gyak6_CK90DC/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak6_CK90DC.MNBServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,22 @@ namespace gyak6_CK90DC
         public Form1()
         {
             InitializeComponent();
+            arfolyam();
+        }
+        public void arfolyam()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }

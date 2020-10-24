36c190b38ccf3683a5fd2776b4a867077f1b44ac
Benjámin Forgács <benjamin.forgacs@stud.uni-corvinus.hu>
2020-10-24 12:39:13 +0200

    class creation

---
+++ gyak7_CK90DC/gyak7_CK90DC/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak7_CK90DC.Entities
+{
+    public class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double ProbabilityOfBirth { get; set; }
+
+    }
+}
---
+++ gyak7_CK90DC/gyak7_CK90DC/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak7_CK90DC.Entities
+{
+    class DeathProbability
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public double ProbabilityOfDead { get; set; }
+    }
+}

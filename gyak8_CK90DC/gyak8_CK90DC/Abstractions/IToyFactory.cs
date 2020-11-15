80a9fc2c754630439c38bf0243bb06399d926004
Benjámin Forgács <benjamin.forgacs@stud.uni-corvinus.hu>
2020-11-15 16:40:36 +0100

    interface

---
+++ gyak8_CK90DC/gyak8_CK90DC/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak8_CK90DC.Abstractions
+{
+    interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}

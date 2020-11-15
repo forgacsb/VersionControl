a58930a2646dbafcb9e3db5539313f3260e2b93f
Benjámin Forgács <benjamin.forgacs@stud.uni-corvinus.hu>
2020-11-15 16:24:51 +0100

    begin absraction

---
+++ gyak8_CK90DC/gyak8_CK90DC/Abstractions/Toy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak8_CK90DC.Abstractions
+{
+   public abstract class Toy: Label
+    {
+        public Toy()
+        {
+            this.AutoSize = false;
+            this.Width = 50;
+            this.Height = 50;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected abstract void DrawImage(Graphics g);
+
+        public virtual void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}

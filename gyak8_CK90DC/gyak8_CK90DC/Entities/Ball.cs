b496f3bb664839c656af9a0466445d1c4d2ac74e
Benjámin Forgács <benjamin.forgacs@stud.uni-corvinus.hu>
2020-11-15 16:27:14 +0100

    abstraction

--- gyak8_CK90DC/gyak8_CK90DC/Entities/Ball.cs
+++ gyak8_CK90DC/gyak8_CK90DC/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak8_CK90DC.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,28 +9,11 @@ using System.Windows.Forms;
 
 namespace gyak8_CK90DC.Entities
 {
-    class Ball : Label
+    class Ball : Toy
     {
-        public Ball()
-        {
-            this.AutoSize = false;
-            this.Width = 50;
-            this.Height = 50;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-
-        public void MoveBall()
-        {
-            Left += 1;
         }
     }
 }

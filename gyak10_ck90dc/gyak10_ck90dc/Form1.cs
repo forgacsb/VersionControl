af965e74537eddaa35a5800c1098d692948468b2
Benjámin Forgács <benjamin.forgacs@stud.uni-corvinus.hu>
2020-11-29 20:17:05 +0100

    #7

--- gyak10_ck90dc/gyak10_ck90dc/Form1.cs
+++ gyak10_ck90dc/gyak10_ck90dc/Form1.cs
@@ -19,6 +19,7 @@ namespace gyak10_ck90dc
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        Brain winnerBrain = null;
 
         public Form1()
         {
@@ -44,6 +45,34 @@ namespace gyak10_ck90dc
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
         }
     }
 }
